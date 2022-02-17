9f54e945b2085551bd6155370c5907bac5fe3973
Rob Booth <rbooth@flipboard.com>
2022-02-17 14:20:07 -0800

    Initial Project Creation

---
+++ DeadmanSwitch/Shared/DeadmanSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeadmanSwitchApp.swift
+//  Shared
+//
+//  Created by Robert Booth on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct DeadmanSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
